2af7ce7f2a0bad73e57f303fd16dbdb1119d52e6
cperdomo12cr <cperdomo@arcetec.com.co>
2024-04-18 13:11:26 -0500

    feat: creacion de rutas para acceder a la vista

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -33,6 +35,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    /* departamentos */
+    Route::resource('departments', DepartmentController::class);
+    /* empleados */
+    Route::resource('employees', EmployeeController::class);
+    /* graficos */
+    Route::get('graphic', [EmployeeController::class, 'EmployeeByDepartament'])->name('graphic');
+    Route::get('reports', [EmployeeController::class, 'reports'])->name('reports');
 });
 
 require __DIR__.'/auth.php';
